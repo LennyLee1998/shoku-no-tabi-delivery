b18982f0a6e462c4033a0e451479a6bce2c06004
Lenny <ly2484579947@gmail.com>
2025-01-30 10:50:12 +0800

    feat: 文件上传
    - alioss

--- sky-common/src/main/java/com/sky/properties/AliOssProperties.java
+++ sky-common/src/main/java/com/sky/properties/AliOssProperties.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "sky.alioss")
+@ConfigurationProperties(prefix = "sky.alioss") //通过 @ConfigurationProperties 的 prefix 属性将配置文件中的值映射到 AliOssProperties 类的对应字段
 @Data
 public class AliOssProperties {
 
