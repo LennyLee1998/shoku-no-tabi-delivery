fbc042de1dea48705b1fd3bbfb63270d59ec3b00
Lenny <ly2484579947@gmail.com>
2025-01-30 19:58:44 +0800

    feat: 删除菜品业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -2,16 +2,17 @@ package com.sky.controller.admin;
 
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Api(tags = "菜品相关接口")
 @RestController
@@ -32,6 +33,32 @@ public class DishController {
     log.info("新增菜品: {}", dishDTO);
     dishService.saveWithFlavor(dishDTO);
     return Result.success();
+  }
 
+  /**
+   * 菜品分页查询
+   * @param dishPageQueryDTO
+   * @return
+   */
+  @GetMapping("/page")
+  @ApiOperation("菜品分页查询")
+  public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO) {
+    log.info("菜品分页查询: {}", dishPageQueryDTO);
+    PageResult pageResult = dishService.page(dishPageQueryDTO);
+    return Result.success(pageResult);
+  }
+
+  /**
+   * 删除菜品
+   * @param ids
+   * @return
+   */
+  @DeleteMapping
+  @ApiOperation("批量删除菜品")
+  public Result delete(@RequestParam List<Long> ids) {
+    //这里传入参数的处理是由springMVC完成的, 所以需要加上@RequestParam的注解, 如果query传入数据和参数类型, 变量名一致,则不需要注解
+    log.info("删除菜品: {}", ids);
+    dishService.deleteBatch(ids);
+    return Result.success();
   }
 }
--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,10 +1,16 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishVO;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface DishMapper {
@@ -24,4 +30,30 @@ public interface DishMapper {
      */
     @AutoFill(OperationType.INSERT)
     void insert(Dish dish);
+
+    /**
+     * 菜品分页查询
+     * @return
+     */
+    Page<DishVO> list(DishPageQueryDTO dishPageQueryDTO);
+
+    /**
+     * 根据id查询菜品
+     * @return
+     */
+    @Select("select * from dish where id = #{id};")
+    Dish queryById(Long id);
+
+    /**
+     * 批量删除菜品
+     * @param ids
+     */
+    void deleteByIds(List<Long> ids);
+
+    /**
+     * 根据id删除菜品
+     * @param id
+     */
+    @Delete("delete from dish where id = #{id}")
+    void deleteById(Long id);
 }
