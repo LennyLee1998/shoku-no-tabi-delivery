7613735fe41684193485088307a85ca6d0016591
Lenny <ly2484579947@gmail.com>
2025-02-05 14:29:27 +0800

    feat: 缓存菜品代码开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -12,9 +12,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @Api(tags = "菜品相关接口")
 @RestController
@@ -23,9 +25,12 @@ import java.util.List;
 public class DishController {
   @Autowired
   private DishService dishService;
+  @Autowired
+  private RedisTemplate redisTemplate;
 
   /**
    * 新增菜品
+   *
    * @param dishDTO
    * @return
    */
@@ -34,11 +39,17 @@ public class DishController {
   public Result save(@RequestBody DishDTO dishDTO) {
     log.info("新增菜品: {}", dishDTO);
     dishService.saveWithFlavor(dishDTO);
+
+    //清理缓存数据
+    String key = "dish_" + dishDTO.getCategoryId();
+    cleanCache(key);
+
     return Result.success();
   }
 
   /**
    * 菜品分页查询
+   *
    * @param dishPageQueryDTO
    * @return
    */
@@ -52,6 +63,7 @@ public class DishController {
 
   /**
    * 删除菜品
+   *
    * @param ids
    * @return
    */
@@ -61,11 +73,16 @@ public class DishController {
     //这里传入参数的处理是由springMVC完成的, 所以需要加上@RequestParam的注解, 如果query传入数据和参数类型, 变量名一致,则不需要注解
     log.info("删除菜品: {}", ids);
     dishService.deleteBatch(ids);
+
+    //清理缓存数据 (由于可能属于某个分类也可能属于不同分类下的菜品, 在缓存的时候就会变得比较复杂,所以这里直接全部删除比较好)
+    cleanCache("dish_*");
+
     return Result.success();
   }
 
   /**
    * 根据id查询菜品
+   *
    * @param id
    * @return
    */
@@ -79,6 +96,7 @@ public class DishController {
 
   /**
    * 修改菜品
+   *
    * @param dishDTO
    * @return
    */
@@ -87,11 +105,18 @@ public class DishController {
   public Result updateDish(@RequestBody DishDTO dishDTO) {
     log.info("修改菜品: {}", dishDTO);
     dishService.updateDish(dishDTO);
+
+    //清理缓存数据(如果修改的是普通的数据, 可以只删除一个
+    // 如果修改的是分类id那么这里redis里面必须把之前的对应键值对删掉还有新改的也删掉
+    //修改操作不是常规操作, 不需要把这里的业务逻辑写得过于复杂, 所以可以都删掉
+    cleanCache("dish_*");
+
     return Result.success();
   }
 
   /**
    * 根据分类id查询菜品
+   *
    * @param categoryId
    * @return
    */
@@ -104,15 +129,28 @@ public class DishController {
   }
 
   /**
-   * 启用、禁用菜品
+   * 启售禁售菜品
    * @param status
    * @param id
    * @return
    */
   @PostMapping("/status/{status}")
-  @ApiOperation("启用禁用菜品")
+  @ApiOperation("启售禁售菜品")
   public Result<String> startOrStop(@PathVariable("status") Integer status, Long id){
     dishService.startOrStop(status,id);
+
+    //这里如果要精确清理的话, 需要做sql查询, 还不如直接删掉
+    cleanCache("dish_*");
+
     return Result.success();
+  }
+
+  /**
+   * 清除缓存数据
+   * @param pattern
+   */
+  private void cleanCache(String pattern) {
+    Set keys = redisTemplate.keys(pattern);
+    redisTemplate.delete(keys);
   }
 }
